a6a8e7f044d22bb5770aff6971cf15cf1fcc951b
Ma Yanlong <l.luffy.river@gmail.com>
2015-11-29 23:24:07 +0800

    regex

---
+++ js正则表达式/scripts/index.js
@@ -0,0 +1,49 @@
+window.onload = function(){
+  // String.match()
+  // String.search()
+  // String.replace()
+  // String.split()
+  // RegExp.exec()
+  // RegExp.test()
+
+  // ^  $  . *  + ? = ! : | \ / ( )  [ ]  { }
+
+  // \t
+  // \n
+  // \r
+  // [...]
+  // [^...]
+  // .
+  // \w
+  // \W
+  // \s
+  // \S
+  // \d
+  // \D
+  // \b
+
+  // {n,m}
+  // {n,}
+  // {n}
+  // ?
+  // +
+  // *
+
+  //匹配2-4个数字
+  //精确匹配三个单词和一个可选的数字
+  //匹配前后带有一个或多个空格的字符串“java"
+  //匹配一个或多个非左括号的字符
+
+  //i g 修饰符
+
+  //search
+  //replace
+  //match
+  //split
+
+  // new RegExp
+  // /abc/g
+  // exec()
+  // test()
+
+};
