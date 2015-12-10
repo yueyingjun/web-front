d63bd307e4dfe3af4ce55c0e06009b71a3c0f754
Ma Yanlong <l.luffy.river@gmail.com>
2015-12-10 10:43:03 +0800

    Update index.js

--- timu/index.js
+++ timu/index.js
@@ -145,5 +145,107 @@ window.onload = function(){
 
 
   //15
-
+  var els = document.getElementsByClassName('cl');
+  els[0].parentElement.removeChild( els[els.length-1] );
+  console.log(els);
+  
+  //16
+  var el = document.getElementById('id');
+  el.onclick = (function(){
+    var i = 0;
+  	return function(){
+  	  i += 1;
+  	  if( i < 3 ){
+  	    alert('click');
+  	  }
+  	}
+  })();
+  
+  //17
+  '1,2,3,4.5:6"7/'.replace(/([,.:"\/])/g,(function(){
+    var dict = {',':'-','.':'--','"':'---','/':'----'};
+    return function(r){
+      return dict[ r[1] ];
+    }
+  })())
+  
+  //18
+  var date = new Date(2015,2,32);
+  console.log( date.getDate() );
+  
+  //19
+  var s = 'woshi,zhong.guo,ren,wo-ai-zi.ji.de-zuo+guo';
+  var t = s.split(/[,.\-\+]/);
+  for(var i = 0; 10; i++){
+    alert( t [ i % t.length ] );
+  }
+  
+  //20
+  for( var i in Array.prototype ){
+    console.log(i);
+  }
+  
+  //21
+  //slice  subStr subString
+  
+  //22
+  //splice  slice
+  
+  //23
+  //parseInt  Number
+  
+  //24
+  var o = {a:1};
+  console.log( o.b.c );
+  console.log( o.b && o.b.c )
+  
+  //25
+  var num = (function(n){
+    if(n === 0) return 1;
+    return n * arguments.callee(n-1)
+  })(4);
+  
+  //26
+  setTimeout(function(){
+    console.log(1);
+    setTimeout(arguments.callee,10);
+  },10)
+  setInterval(function(){
+    console.log(1);
+  },10)
+  
+  //27
+  var arr = [3,5,6,9,12,32,1,2];
+  Array.prototype.y = function(){
+    var arr = [], r = [], random, dict={};
+    while(arr.length != this.length ){
+      random = Math.floor(Math.random()*this.length);
+      if( !dict[random] ){
+        arr.push(random);
+        dict[random] = true;
+      }
+    }
+    for(var i = 0 ; i<arr.length; i++){
+      r.push( this[ arr[i] ] );
+    }
+    return r;
+  }
+  var c = arr.y();
+  console.log(c);
+  
+  //28
+  var o = {a:1,b:2,c:3,d:{a:1,b:2,c:{a:1,b:2}}};
+  Object.prototype.z = function(){
+    var n = {};
+    for( var i in this ){
+      if( typeof i  == 'object' ){
+        arguments.callee(i);
+      }else{
+        n[i] = this[i];
+      }
+    }
+    return n;
+  }
+  var n = o.z();
+  
 }
